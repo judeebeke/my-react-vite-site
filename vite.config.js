ed038c8c51f32b845bf42574473f0d80fedc2ba3
Jude Ebeke <amazingtechiegenie@gmail.com>
2023-05-16 00:49:46 +0100

    fix output build dir

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import tailwindcss from "tailwindcss"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), postcss(), tailwindcss()],
+   build: {
+    outDir: 'dist'
+  }
 });
 
 
