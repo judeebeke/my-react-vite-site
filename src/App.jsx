8277d8769347e394ea0a1d10716313442ece34de
Jude Ebeke <amazingtechiegenie@gmail.com>
2023-05-16 02:04:28 +0100

    Removed Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect, Fragment } from "react";
+import {  Fragment } from "react";
 import Layout from "./components/Layout/Layout";
 import Hero from "./components/Layout/desktop/Hero";
-import { ScaleLoader } from "react-spinners";
+// import { ScaleLoader } from "react-spinners";
 
 import DesktopNav from "./components/navigation/DesktopNav";
 import About from "./components/Layout/About";
@@ -11,27 +11,22 @@ import PastProjects from "./components/Layout/PastProjects";
 import MobileNav from "./components/navigation/MobileNav";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    window.addEventListener("load", () => {
-      return setLoading(false);
-    });
+  // useEffect(() => {
+  //   window.addEventListener("load", () => {
+  //     return setLoading(false);
+  //   });
 
-    return () => {
-      window.removeEventListener("load", () => {
-        return setLoading(false);
-      });
-    };
-  }, []);
+  //   return () => {
+  //     window.removeEventListener("load", () => {
+  //       return setLoading(false);
+  //     });
+  //   };
+  // }, []);
 
   return (
-    <>
-      {loading ? (
-        <div className="flex h-screen justify-center items-center">
-          <ScaleLoader color="#0678E3" size={100} />
-        </div>
-      ) : (
+    
         <Fragment>
           <DesktopNav />
           <MobileNav />
@@ -42,9 +37,7 @@ function App() {
             <Resume />
             <PastProjects />
           </Layout>
-        </Fragment>
-      )}
-    </>
+        </Fragment>  
   );
 }
 
