d322e31f116cc9b7136e5350fe8b77ade04dfcbd
Jude Ebeke <amazingtechiegenie@gmail.com>
2023-05-21 08:35:35 +0100

    Project web view fixes

--- src/components/projects/WebDevProjects.jsx
+++ src/components/projects/WebDevProjects.jsx
@@ -1,51 +1,80 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import { flexStyle } from '../../style';
-import { AiOutlineLink, AiOutlineGithub, AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import { flexStyle } from "../../style";
+import {
+  AiOutlineLink,
+  AiOutlineGithub,
+  AiOutlineEye,
+  AiOutlineEyeInvisible,
+} from "react-icons/ai";
 import { webProjectImage } from "../localstore/LocalData";
 
 const WebDevProjects = () => {
   const [viewProjectDetails, setViewProject] = useState(false);
+  const [currentViewProject, setCurrentViewProject] = useState(0);
 
-  const viewProjectDetailsHandler = (bool) => {
+  const viewProjectDetailsHandler = (bool, id) => {
     setViewProject(bool);
-  }
+    console.log(bool, id)
+    setCurrentViewProject(id)
+  };
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mb-20">
       {webProjectImage.map((data) => {
         return (
-            <figure key={data.id}  className="relative max-w-lg max-h-lg h-48 text-dark dark:text-white lg:w-80">
-              <div className={`absolute top-0 left-0 w-full h-full ${viewProjectDetails ? "p-4 text-base font-medium overflow-auto transition-all ease-linear": `font-bold text-center text-3xl ${flexStyle} flex-col gap-y-6 transition-all ease-linear`} opacity-0 hover:opacity-70 focus:opacity-70 hover:bg-white dark:bg-dark`}>
-                {viewProjectDetails ? <div onMouseLeave={()=>{viewProjectDetailsHandler(false)}}>
+          <figure
+            key={data.id}
+            className="relative max-w-lg max-h-lg text-dark lg:w-80"
+          >
+           {!viewProjectDetails &&  <div
+              className={`absolute top-0 left-0 w-full h-full font-bold text-center text-3xl ${flexStyle} flex-col gap-y-6 opacity-0 transition-all ease-linear duration-300 hover:opacity-70 focus:opacity-70 hover:bg-white focus:bg-white`}
+              onMouseLeave={() => {
+                viewProjectDetailsHandler(false, 0);
+              }}
+            >
+              
+                <>
+                  <button
+                    onClick={() => {
+                      viewProjectDetailsHandler(true, data.id);
+                    }}
+                  >
+                    <AiOutlineEye />
+                  </button>
+                  <div className={`${flexStyle} gap-x-10`}>
+                    <a href={data.url} target="_blank" rel="noreferrer">
+                      <AiOutlineLink />
+                    </a>
+                    <a href={data.github} target="_blank" rel="noreferrer">
+                      <AiOutlineGithub />
+                    </a>
+                  </div>
+                </>
+            </div>
+              }
+            {data.id === currentViewProject && <div className={`${viewProjectDetails ? "flex flex-col " : "hidden" } absolute top-0 left-0 w-full h-full p-4 text-base font-medium overflow-auto  opacity-70 text-dark dark:text-white bg-white dark:bg-dark`}  onMouseLeave={() => {
+                viewProjectDetailsHandler(false, 0);
+              }}>
                   <span className='w-full flex justify-between text-white text-lg bg-lowDark items-center px-2 py-1'>
                     <p>{data.title}</p>
-                    <button onClick={()=>{viewProjectDetailsHandler(false)}}><AiOutlineEyeInvisible /></button>
+                    <button onClick={()=>{viewProjectDetailsHandler(false, 0)}}><AiOutlineEyeInvisible /></button>
                   </span>
                   <p className='pt-1'><strong>Description: </strong>{data.description}
                     <br/><br />
                     <strong>Improvements: </strong> {data.improvement}
                   </p>
-                </div> : 
-                <>
-                <button onClick={()=>{viewProjectDetailsHandler(true)}}><AiOutlineEye /></button>
-                <div className={`${flexStyle} gap-x-10`}>
-                <a href={data.url} target='_blank' rel="noreferrer"><AiOutlineLink /></a>
-                <a href={data.github} target='_blank' rel="noreferrer"><AiOutlineGithub /></a>
-                </div>
-                </>
-                }
-              </div>
-              <img
-                src={data.image}
-                className="w-full h-full object-cover"
-                alt="My Graphics Projects"
-              />
-            </figure>
-          );
-        })}
+                </div> }
+            <img
+              src={data.image}
+              className="w-full h-full object-cover"
+              alt="My Graphics Projects"
+            />
+          </figure>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default WebDevProjects;
